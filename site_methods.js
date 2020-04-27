6dc5ec9007177ae9f086d28519324a839000f227
Alvin Lee <algrn91@gmail.com>
2020-04-26 21:40:00 -0700

    Add photography and contact section

--- site_methods.js
+++ site_methods.js
@@ -9,23 +9,57 @@ $(window).scroll(function () {
 
 // Smooth scrolling
 $(document).ready(function ($) {
-    $(".smoothscroll").on("click", function (e) {
-      e.preventDefault();
-  
-      var target = this.hash,
-        $target = $(target);
-  
-      $("html, body")
-        .stop()
-        .animate(
-          {
-            scrollTop: $target.offset().top,
-          },
-          800,
-          "swing",
-          function () {
-            window.location.hash = target;
-          }
-        );
-    });
+  $(".smoothscroll").on("click", function (e) {
+    e.preventDefault();
+
+    var target = this.hash,
+      $target = $(target);
+
+    $("html, body")
+      .stop()
+      .animate(
+        {
+          scrollTop: $target.offset().top
+        },
+        800,
+        "swing",
+        function () {
+          window.location.hash = target;
+        }
+      );
   });
+});
+
+// Photo slideshow
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function nextSlide(n) {
+  showSlides((slideIndex += n));
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides((slideIndex = n));
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("photo-slide");
+  //var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  //for (i = 0; i < dots.length; i++) {
+  //  dots[i].className = dots[i].className.replace(" active", "");
+  //}
+  slides[slideIndex - 1].style.display = "block";
+  //dots[slideIndex - 1].className += " active";
+}
